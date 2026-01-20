5e3c71981b314d1274e171eea4abd4ee5d88eee7
Maulana Aprizqy Sumaryanto <maulana4pz@gmail.com>
2026-01-20 12:24:25 +0700

    Feat: Add toast notifications for service worker updates

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -1,11 +1,18 @@
 import { registerSW } from 'virtual:pwa-register'
+import { toast } from 'vue-sonner'
 
-registerSW({
+const updateSW = registerSW({
   immediate: true,
   onNeedRefresh() {
-    // console.log('onNeedRefresh')
+    toast.info('New content available', {
+      description: 'Click to reload the application',
+      action: {
+        label: 'Reload',
+        onClick: () => updateSW(true)
+      }
+    })
   },
   onOfflineReady() {
-    // console.log('onOfflineReady')
+    toast.success('App ready to work offline')
   }
 })
